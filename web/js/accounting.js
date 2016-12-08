4491666f33df943e4dc4e2a2a5861ea18200508f
Rui Vilares <rui.vilares@hotmail.com>
2016-12-08 20:27:51 +0000

    Accounting page improvement

--- web/js/accounting.js
+++ web/js/accounting.js
@@ -8,13 +8,11 @@ $(document).ready(function(){
       for(var i = 0; i < result.length; i++){
           var ob = result[i];
 
-          $(".balanceteItems").append('<tr>');
-          $(".balanceteItems").append('<td>' + ob.codConta + '</td>');
-          $(".balanceteItems").append('<td>' + ob.nomeConta + '</td>');
-          $(".balanceteItems").append('<td>' + ob.credito + '</td>');
-          $(".balanceteItems").append('<td>' + ob.debito + '</td>');
-          $(".balanceteItems").append('<td>' + ob.saldo + '</td>');
-          $(".balanceteItems").append('</tr>');
+          $(".balanceteItems").append('<tr><td>' + ob.codConta + '</td>' +
+          "<td>" + ob.nomeConta + '</td>' +
+          "<td class='text-right'>" + ob.credito.toFixed(2) + '</td>' +
+          "<td class='text-right'>" + ob.debito.toFixed(2) + '</td>' +
+          "<td class='text-right'>" + ob.saldo.toFixed(2) + '</td></tr>');
 
           $(".loading").addClass('hidden');
       }
@@ -26,108 +24,128 @@ $(document).ready(function(){
         for(var i = 0; i < result[0].length; i++){
             anc += parseInt(result[0][i]['m_Item2']);
         }
-        $(".balancoItems").append('<tr>');
-        $(".balancoItems").append('<td>' + '<strong>Activos não correntes</strong>' + '</td>');
-        $(".balancoItems").append('<td>' +  anc + '</td>');
-        $(".balancoItems").append('</tr>');
-    
+
+        $(".balancoItems").append("<tr>" +
+              "<td class='text-center'> ATIVO</td>" +
+              "<td></td></tr>");
+
+        $(".balancoItems").append("<tr class='active'>" +
+              "<td>Ativo não corrente</td>" +
+              "<td></td></tr>");
+
     for (var i = 0; i < result[0].length; i++){
       if(result[0][i]['m_Item2'] != 0){
-          $(".balancoItems").append('<tr>');
-          $(".balancoItems").append('<td>' + result[0][i]["m_Item1"] + '</td>');
-          $(".balancoItems").append('<td>' + result[0][i]["m_Item2"] + '</td>');
-          $(".balancoItems").append('</tr>');
+        $(".balancoItems").append("<tr><td>" + result[0][i]["m_Item1"] +
+            "</td><td class='text-right'>" + result[0][i]["m_Item2"].toFixed(2) + "</td></tr>");
         }
     }
+
+          $(".balancoItems").append("<tr>" +
+              "<td class='text-right'>Subtotal</td>" +
+              "<td class='text-right'>" +  anc.toFixed(2) + "</td></tr>");
 
         var ac = 0;
         for(var i = 0; i < result[1].length; i++){
             ac += parseInt(result[1][i]['m_Item2']);
         }
-        $(".balancoItems").append('<tr>');
-        $(".balancoItems").append('<td>' + '<strong>Activos correntes</strong>' + '</td>');
-        $(".balancoItems").append('<td>' +  ac + '</td>');
-        $(".balancoItems").append('</tr>');
-    
+
+        $(".balancoItems").append("<tr class='active'>" +
+              "<td>Ativo corrente</td>" +
+              "<td></td></tr>");
+
     for (var i = 0; i < result[1].length; i++){
         if(result[1][i]['m_Item2'] != 0){
-          $(".balancoItems").append('<tr>');
-          $(".balancoItems").append('<td>' + result[1][i]["m_Item1"] + '</td>');
-          $(".balancoItems").append('<td>' + result[1][i]["m_Item2"] +'€'+ '</td>');
-          $(".balancoItems").append('</tr>');          
+          $(".balancoItems").append("<tr><td>" + result[1][i]["m_Item1"] +
+              "</td><td class='text-right'>" + result[1][i]["m_Item2"].toFixed(2) + "</td></tr>");
       }
     }
+          $(".balancoItems").append("<tr>" +
+            "<td class='text-right'>Subtotal</td>" +
+            "<td class='text-right'>" +  ac.toFixed(2) + "</td></tr>");
 
-        $(".balancoItems").append('<tr>');
-        $(".balancoItems").append('<td>' + '<strong>Total do ativo</strong>' + '</td>');
-        $(".balancoItems").append('<td>' +  (anc + ac) + '</td>');
-        $(".balancoItems").append('</tr>');
+          var ta = anc + ac;
+          $(".balancoItems").append("<tr>" +
+            "<td class='text-right'>Total do ativo</td>" +
+            "<td class='text-right'>" +  ta.toFixed(2) + "</td></tr>");
+
+
+        $(".balancoItems").append("<tr>" +
+              "<td class='text-center'> CAPITAL PROPRIO E PASSIVO</td>" +
+              "<td></td></tr>");
 
         var cp = 0;
         for(i = 0; i < result[2].length; i++){
-            cp += parseInt(result[2][i]['m_Item2']);
+
+          cp += parseInt(result[2][i]['m_Item2']);
         }
-        $(".balancoItems").append('<tr>');
-        $(".balancoItems").append('<td>' + '<strong>Capital próprio</strong>' + '</td>');
-        $(".balancoItems").append('<td>' +  cp + '</td>');
-        $(".balancoItems").append('</tr>');
+
+        $(".balancoItems").append("<tr class='active'>" +
+              "<td>Capital próprio</td>" +
+              "<td></td></tr>");
 
       for (i = 0; i < result[2].length; i++){
         if(result[2][i]['m_Item2'] !== 0){
-          $(".balancoItems").append('<tr>');
-          $(".balancoItems").append('<td>' + result[2][i]["m_Item1"] + '</td>');
-          $(".balancoItems").append('<td>' + result[2][i]["m_Item2"] +'€'+ '</td>');
-          $(".balancoItems").append('</tr>');          
+          $(".balancoItems").append("<tr><td>" + result[2][i]["m_Item1"] +
+              "</td><td class='text-right'>" + result[2][i]["m_Item2"].toFixed(2) + "</td></tr>");
         }
     }
+
+        $(".balancoItems").append("<tr>" +
+            "<td class='text-right'>Total do capital próprio</td>" +
+            "<td class='text-right'>" +  cp.toFixed(2) + "</td></tr>");
 
 
       var pnc = 0;
         for(i = 0; i < result[3].length; i++){
             pnc += parseInt(result[3][i]['m_Item2']);
         }
-        $(".balancoItems").append('<tr>');
-        $(".balancoItems").append('<td>' + '<strong>Passivos não correntes</strong>' + '</td>');
-        $(".balancoItems").append('<td>' +  pnc + '</td>');
-        $(".balancoItems").append('</tr>');
+
+        $(".balancoItems").append("<tr class='active'>" +
+              "<td>Passivo</td>" +
+              "<td></td></tr>");
+
+        $(".balancoItems").append("<tr class='active'>" +
+              "<td>Passivo não corrente</td>" +
+              "<td></td></tr>");
 
       for (i = 0; i < result[3].length; i++){
         if(result[3][i]['m_Item2'] !== 0){
-          $(".balancoItems").append('<tr>');
-          $(".balancoItems").append('<td>' + result[3][i]["m_Item1"] + '</td>');
-          $(".balancoItems").append('<td>' + result[3][i]["m_Item2"] +'€'+ '</td>');
-          $(".balancoItems").append('</tr>');          
+          $(".balancoItems").append("<tr><td>" + result[3][i]["m_Item1"] +
+              "</td><td class='text-right'>" + result[3][i]["m_Item2"].toFixed(2) + "</td></tr>");
       }
     }
+
+        $(".balancoItems").append("<tr>" +
+            "<td class='text-right'>Subtotal</td>" +
+            "<td class='text-right'>" +  pnc.toFixed(2) + "</td></tr>");
 
       var pc = 0;
         for(i = 0; i < result[4].length; i++){
             pc += parseInt(result[4][i]['m_Item2']);
         }
-        $(".balancoItems").append('<tr>');
-        $(".balancoItems").append('<td>' + '<strong>Passivos correntes</strong>' + '</td>');
-        $(".balancoItems").append('<td>' +  pc + '</td>');
-        $(".balancoItems").append('</tr>');
+        $(".balancoItems").append("<tr class='active'>" +
+              "<td>Passivo corrente</td>" +
+              "<td></td></tr>");
 
       for (i = 0; i < result[4].length; i++){
         if(result[4][i]['m_Item2'] !== 0){
-          $(".balancoItems").append('<tr>');
-          $(".balancoItems").append('<td>' + result[4][i]["m_Item1"] + '</td>');
-          $(".balancoItems").append('<td>' + result[4][i]["m_Item2"] +'€'+ '</td>');
-          $(".balancoItems").append('</tr>');          
+          $(".balancoItems").append("<tr><td>" + result[4][i]["m_Item1"] +
+              "</td><td class='text-right'>" + result[4][i]["m_Item2"].toFixed(2) + "</td></tr>");
       }
     }
 
-     $(".balancoItems").append('<tr>');
-     $(".balancoItems").append('<td>' + '<strong>Total do passivo</strong>' + '</td>');
-     $(".balancoItems").append('<td>' +  (pnc+pc) + '</td>');
-     $(".balancoItems").append('</tr>');
+    $(".balancoItems").append("<tr>" +
+        "<td class='text-right'>Subtotal</td>" +
+        "<td class='text-right'>" +  pc.toFixed(2) + "</td></tr>");
 
-     $(".balancoItems").append('<tr>');
-     $(".balancoItems").append('<td>' + '<strong>Total do passivo e capital próprio</strong>' + '</td>');
-     $(".balancoItems").append('<td>' +  (pnc+pc+cp) + '</td>');
-     $(".balancoItems").append('</tr>');
+    var tp = pnc+pc;
+    $(".balancoItems").append("<tr>" +
+        "<td class='text-right'>Total do passivo</td>" +
+        "<td class='text-right'>" + tp.toFixed(2) + "</td></tr>");
 
-
+    var tcpp = pnc+pc+cp;
+    $(".balancoItems").append("<tr>" +
+        "<td class='text-right'>Total do capital próprio e do passivo</td>" +
+        "<td class='text-right'>" +  tcpp.toFixed(2) + "</td></tr>");
   }});
 });
